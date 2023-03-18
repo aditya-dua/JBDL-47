7d2e3fc638e4f028bc3f7a776b7f52ccef162922
Aditya Dua <adityaduatechm@gmail.com>
2023-03-18 21:45:26 +0530

    File Write
    File Write

--- L7_File_Handling/src/FileSample.java
+++ L7_File_Handling/src/FileSample.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class FileSample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		File f = new File("myFirstFile.txt");
 		
@@ -26,6 +26,32 @@ public class FileSample {
 		System.out.println(f.isDirectory());
 		
 		
+		File file = new File("folder1/folder2/folder3");
 		
+		// Log file
+		// File f = new file("03-2022/18")
+	
+		if(file.mkdirs()) {
+			System.out.println("Folder creation is success");
+		}else {
+			System.out.println("Folder already exists");
+		}
+		
+		File fileNew = new File(file.getAbsolutePath()+"/myNewFile.txt");
+		
+		
+		System.out.println(fileNew.createNewFile());
+		System.out.println(fileNew.getAbsolutePath());
+		
+		
+		File home = new File("/");
+		
+		String[] fileList = home.list();
+		for (String string : fileList) {
+			
+			System.out.println("File: "+string);
+		}
+		
+
 	}
 }
---
+++ L7_File_Handling/src/FileWriteExampleMain.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriteExampleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+
+		try {
+			File f = new File("myFirstFile.txt");
+			FileWriter fileWriter;
+			
+			fileWriter = new FileWriter(f);
+			fileWriter.write("Its my First Text File");
+			
+			fileWriter.flush();
+			fileWriter.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
