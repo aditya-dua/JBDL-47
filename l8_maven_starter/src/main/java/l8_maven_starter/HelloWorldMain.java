17fa4f07b592e917732c10c355dcf0be43bc513f
Aditya Dua <adityaduatechm@gmail.com>
2023-03-25 19:56:40 +0530

    Maven_Getting Started
    Maven_Getting Started

---
+++ l8_maven_starter/src/main/java/l8_maven_starter/HelloWorldMain.java
@@ -0,0 +1,16 @@
+package l8_maven_starter;
+
+public class HelloWorldMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+	/*
+	 * Basic understanding of Spring
+	 * DoGet and doPost method of servelets
+	 * web.xml
+	 * Collection Assignment
+	 */
+}
