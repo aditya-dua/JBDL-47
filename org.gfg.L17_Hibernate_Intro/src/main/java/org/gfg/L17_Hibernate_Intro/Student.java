fa84841681172444a79f9ee3873df087708a8b12
Aditya Dua <adityaduatechm@gmail.com>
2023-04-23 22:30:17 +0530

    Annotation Hibernate Example
    Annotation Hibernate Example

---
+++ org.gfg.L17_Hibernate_Intro/src/main/java/org/gfg/L17_Hibernate_Intro/Student.java
@@ -0,0 +1,46 @@
+package org.gfg.L17_Hibernate_Intro;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "jbdl47-student")
+
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+}
--- org.gfg.L17_Hibernate_Intro/src/main/java/org/gfg/L17_Hibernate_Intro/StudentAnnotationMain.java
+++ org.gfg.L17_Hibernate_Intro/src/main/java/org/gfg/L17_Hibernate_Intro/StudentAnnotationMain.java
@@ -4,20 +4,20 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class MappingXMLEmployeeMain {
+public class StudentAnnotationMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Configuration c = new Configuration();
 		
-		SessionFactory factory =c.configure("hbm.cfg.xml").buildSessionFactory();
+		SessionFactory factory =c.configure("hbm-student-annotation.cfg.xml").buildSessionFactory();
 		
-		Employee e = new Employee(1, "ABC");
+		Student student = new Student(100, "ADITYA");
 		
 		Session s = factory.openSession();
 		
-		s.save(e);
+		s.save(student);
 		s.close();
 		//hbm-student-annotation.cfg.xml
 	}
