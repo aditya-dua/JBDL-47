47501ee88d7f7d35b5e610b5a4bde736bceea6c1
Aditya Dua <adityaduatechm@gmail.com>
2023-05-13 21:59:33 +0530

    Create TopicListener.java

---
+++ l20_kafka/src/main/java/org/gfg/kafka/l20_kafka/TopicListener.java
@@ -0,0 +1,24 @@
+package org.gfg.kafka.l20_kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicListener {
+
+	private String topic = "hello-world";
+	
+	
+	@KafkaListener(topics = "hello-world",groupId = "group_id" )
+	public void consume(ConsumerRecord<String, String> payload) {
+		
+		System.out.println("Topic Name = "+payload.topic());
+		System.out.println("Key "+payload.key());
+		System.out.println("Value "+payload.value());
+		System.out.println("Entire Payload"+payload.toString());
+
+	}
+	
+	
+}
